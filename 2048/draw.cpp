27cdb24dbe69c7e8a5aa86cb26db81ec4dacc543
hc <2414767656@qq.com>
2017-04-07 15:43:29 +0800

    add output message

--- 2048/draw.cpp
+++ 2048/draw.cpp
@@ -37,9 +37,10 @@ void draw() {
 		cout << endl;
 		draw_line();
 	}
-	cout << endl << endl;
+	cout << endl;
 	for (int i = 0; i < ROW / 2; i++) {
 		cout << "  ";
 	}
-	cout << "score: " << score << endl;
+	cout << "score: " << score << endl << endl;
+	cout << "press W A S D to move numbers.\npress F to restart, Q to exit." << endl;
 }
